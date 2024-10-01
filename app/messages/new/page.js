87804e6a7bdafcab98cb480da64361264f4be151
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-01 12:08:02 +0200

    Managing Data Cache in newMessages

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 
 import { addMessage } from '@/lib/messages';
+import { revalidatePath } from 'next/cache';
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
@@ -8,6 +9,7 @@ export default function NewMessagePage() {
 
     const message = formData.get('message');
     addMessage(message);
+    //revalidatePath(message)
     redirect('/messages');
   }
 
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,17 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from 'next/cache';
+
+// export const revalidate = 5;
+// export const dynamic = 'force-dynamic';
+
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  unstable_noStore();
+  const response = await fetch('http://localhost:8080/messages'/*, {
+    next :{
+      revalidate: 5,
+    }
+  }*/ );
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {

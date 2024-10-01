d509a330850fcf4242cb7e5f3cfc6048d9df7108
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-01 15:14:01 +0200

    Making Sense Of The Full Route Cache

--- app/messages/page.js
+++ app/messages/page.js
@@ -2,10 +2,14 @@ import Messages from '@/components/messages';
 import { unstable_noStore } from 'next/cache';
 
 // export const revalidate = 5;
-// export const dynamic = 'force-dynamic';
+
+
+// Ne pas mettre en cache sur l'ensemble du fichier
+//export const dynamic = 'force-dynamic';
 
 
 export default async function MessagesPage() {
+  // Ne pas mettre en cache juste sur ce module
   unstable_noStore();
   const response = await fetch('http://localhost:8080/messages'/*, {
     next :{

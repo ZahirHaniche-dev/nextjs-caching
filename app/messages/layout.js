b3f3505874e80fcbf2b900695fe06461b3b69786
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-01 15:30:06 +0200

    Get Data in BDD and display

--- app/messages/layout.js
+++ app/messages/layout.js
@@ -1,10 +1,14 @@
+import { getMessages } from '@/lib/messages';
+
 export default async function MessagesLayout({ children }) {
-  const response = await fetch('http://localhost:8080/messages', {
+  /*const response = await fetch('http://localhost:8080/messages', {
     headers: {
       'X-ID': 'layout',
     },
-  });
-  const messages = await response.json();
+  });*/
+  //const messages = await response.json();
+
+  const messages = getMessages();
   const totalMessages = messages.length;
 
   return (
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,4 +1,5 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
 import { unstable_noStore } from 'next/cache';
 
 // export const revalidate = 5;
@@ -8,15 +9,17 @@ import { unstable_noStore } from 'next/cache';
 //export const dynamic = 'force-dynamic';
 
 
-export default async function MessagesPage() {
+export default function MessagesPage() {
   // Ne pas mettre en cache juste sur ce module
-  unstable_noStore();
-  const response = await fetch('http://localhost:8080/messages'/*, {
-    next :{
-      revalidate: 5,
-    }
-  }*/ );
-  const messages = await response.json();
+ // unstable_noStore();
+ // const response = await fetch('http://localhost:8080/messages'/*, {
+  //  next :{
+   //   revalidate: 5,
+   // }
+  //}*/ );
+  // messages = await response.json();
+
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
